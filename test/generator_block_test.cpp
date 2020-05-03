57f9f5f476f1ba6298fb54e519e39c85aa31ecdb
atoka <makrusan@gmail.com>
2020-05-03 10:34:16 +0300

    Add CsvReaderBlock tests

--- test/generator_block_test.cpp
+++ test/generator_block_test.cpp
@@ -43,26 +43,50 @@ namespace pipeliner {
     };
 
     TEST_CASE("CsvReaderBlock", "[GeneratorBlock]") {
-        SECTION("Valid file") {
-            std::vector<Uint8> expected{
-                    1, 114, 102, 69, 4, 169, 113, 58, 172, 115, 88, 177, 199, 212, 159, 22,
-                    130, 233, 72, 31, 121, 34, 226, 49, 235, 214, 17, 120, 146, 115, 226, 143,
-                    102, 189, 20, 86, 131, 201, 29, 67, 253, 65, 102, 143, 181, 101, 70, 228,
-                    145, 35, 131, 221, 164, 134, 140, 249, 149, 75, 252, 59, 69, 7, 99, 179,
-                    218, 242, 204, 3, 77, 32, 18, 42, 202, 161, 112, 226, 206, 51, 6, 43};
+        SECTION("Reading 16 columns, 100 rows csv file") {
+            const auto csvFile = std::filesystem::path{} / "data" / "MOCK_DATA_16_100.csv";
 
-            auto csvFile = std::filesystem::path{} / "data" / "MOCK_DATA_16_100.csv";
-            CsvReaderBlock block{csvFile};
+            SECTION("Check first 5 rows of csv file") {
+                std::vector<Uint8> expected{
+                        1, 114, 102, 69, 4, 169, 113, 58, 172, 115, 88, 177, 199, 212, 159, 22,
+                        130, 233, 72, 31, 121, 34, 226, 49, 235, 214, 17, 120, 146, 115, 226, 143,
+                        102, 189, 20, 86, 131, 201, 29, 67, 253, 65, 102, 143, 181, 101, 70, 228,
+                        145, 35, 131, 221, 164, 134, 140, 249, 149, 75, 252, 59, 69, 7, 99, 179,
+                        218, 242, 204, 3, 77, 32, 18, 42, 202, 161, 112, 226, 206, 51, 6, 43};
 
-            std::vector<Uint8> actual{};
-            while (expected.size() != actual.size()) {
-                auto chunk = block.processChunk(nullptr);
-                actual.push_back(chunk->data1);
-                actual.push_back(chunk->data2);
+                CsvReaderBlock block{csvFile};
+
+                std::vector<Uint8> actual{};
+                while (expected.size() != actual.size()) {
+                    auto chunk = block.processChunk(nullptr);
+                    actual.push_back(chunk->data1);
+                    actual.push_back(chunk->data2);
+                }
+
+                REQUIRE(expected == actual);
             }
 
-            REQUIRE(expected == actual);
+            SECTION("Read file until the end") {
+                CsvReaderBlock block{csvFile};
+                int elementsCount{0};
+                while (true) {
+                    auto chunk = block.processChunk(nullptr);
+                    if (chunk->getType() == DataChunk::End) {
+                        break;
+                    }
+                    elementsCount += 2;
+                }
+                // 16 columns, 100 rows
+                REQUIRE(16 * 100 == elementsCount);
+            }
         }
     }
+
+    class RandomNumberGeneratorBlock : public BasicBlock {
+    public:
+        RandomNumberGeneratorBlock() : BasicBlock{nullptr} {
+
+        }
+    };
 
 }
