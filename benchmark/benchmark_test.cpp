c172d4bbfd820554ac8eaa4aa39beed868218121
atoka <makrusan@gmail.com>
2020-07-10 22:54:36 +0300

    Improve benchmark readability
    Now benchmark explicitly counts time of ComputationBlock iteration.

--- benchmark/benchmark_test.cpp
+++ benchmark/benchmark_test.cpp
@@ -5,31 +5,38 @@
 
 namespace pipeliner {
 
+    class PipelinerBenchmark : public hayai::Fixture {
+    public:
+        void SetUp() override {
+            const auto generatorBlockDelay = 0ms;
+            const int threshold = 128;
+
+            const int width = 10;
+            const int rowCount = 10;
+
+            b1_ = std::make_unique<RandomNumberGeneratorBlock>(generatorBlockDelay);
+            b2_ = std::make_unique<FilterBlock>(threshold, width + 8, b1_.get());
+            b3_ = std::make_unique<LabellingBlock>(width, b2_.get());
+            b4_ = std::make_unique<ComputationBlock>(b3_.get());
+            b4_->start();
+        }
+
+        void TearDown() override {
+            b4_->stop();
+        }
+
+        std::unique_ptr<RandomNumberGeneratorBlock> b1_;
+        std::unique_ptr<FilterBlock> b2_;
+        std::unique_ptr<LabellingBlock> b3_;
+        std::unique_ptr<ComputationBlock> b4_;
+    };
+
     // Quote from the Hayai documentation at https://bruun.co/2012/02/07/easy-cpp-benchmarking
     //
     // > The third and fourth parameters specify the number of runs and iterations to perform.
     // > A run is the execution of the code in the brackets performed iterations number of times.
-    //
-    // To calcutate time to process one chunk use formula:
-    // T = AverageTime / rowCount / width * 2
-    //     ^                                ^
-    //     from benchmark output            two elements per chunk
-
-    BENCHMARK(AllBlocks, RandomNumberGenerator, 10, 1) {
-        const auto generatorBlockDelay = 0ms;
-        const int threshold = 128;
-
-        const int width = 10;
-        const int rowCount = 10;
-
-        RandomNumberGeneratorBlock b1{generatorBlockDelay};
-        FilterBlock b2{threshold, width + 8, &b1};
-        LabellingBlock b3{width, &b2};
-        ComputationBlock b4{&b3};
-
-        b4.start();
-        for (int i{0}; i != width * rowCount; ++i) { b4.waitChunk(); }
-        b4.stop();
+    BENCHMARK_F(PipelinerBenchmark, _, 10, 1000) {
+        b4_->waitChunk();
     }
 
 }
